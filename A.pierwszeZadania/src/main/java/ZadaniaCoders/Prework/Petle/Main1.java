7038ce08d3287ca3ec43bef6d77724f968e52031
Marta Rembelska <martarembelska@pc46.home>
2018-06-06 22:06:42 +0200

    Main1 petle

---
+++ A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main1.java
@@ -0,0 +1,23 @@
+package ZadaniaCoders.Prework.Petle;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int nr1 = 1;
+        int nr2 = 2;
+
+        if (nr1 < nr2) {
+            System.out.println("Większa liczba to: " + nr2);
+        } else {
+            System.out.println("Większa liczba to: " + nr1);
+
+        }
+
+
+    }
+}
+
+/*
+1. W pliku `Main1.java` stwórz dwie zmienne, o nazwach `nr1, nr2`, przechowujące liczby całkowite.
+2. Następnie za pomocą instrukcji warunkowej ```if ... else ```, wypisz w konsoli, która z nich jest większa.
+3. Wypisany string ma być wg. wzoru. `Większa liczba to 7.`
+ */
