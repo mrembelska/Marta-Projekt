d4dbaae2b2bf67f8c042c72615aad4c477b828f0
Marta Rembelska <martarembelska@pc46.home>
2018-06-12 21:22:14 +0200

    zadanie3 i 4

--- A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main3.java
+++ A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main3.java
@@ -1,4 +1,19 @@
 package ZadaniaCoders.Prework.Petle;
 
 public class Main3 {
+    public static void main(String[] args) throws java.lang.Exception {
+        String s ="Lubię javę";
+
+        for (int i =0; i<10; i++){
+
+            System.out.println(s);
+        }
+
+
+    }
 }
+/*
+1. W pliku `Main3.java` stwórz pętle, która 10 razy wypisze w konsoli tekst "Lubię Java".
+2. Napisz zarówno pętle `for` jak i pętlę `while`.
+3. W sumie napis będzie widoczny 20 razy.
+ */
---
+++ A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main4.java
@@ -0,0 +1,10 @@
+package ZadaniaCoders.Prework.Petle;
+
+public class Main4 {
+    public static void main(String[] args){
+
+        for (int i = 1; i<=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
