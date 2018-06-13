fa0124728565daf86d981cc45770d6acae0bec71
Marta Rembelska <martarembelska@pc46.home>
2018-06-13 19:01:44 +0200

    Main7

---
+++ A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main7.java
@@ -0,0 +1,19 @@
+package ZadaniaCoders.Prework.Petle;
+
+public class Main7 {
+    public static void main(String[] args) {
+        for ( int i =0; i < 10; i = i + 1) {
+            for (int j = 0; j < 10; j = j+ 1){
+                System.out.println("I = " + i + " " + "J= " + j);
+
+            }
+        }
+
+    }
+}
+
+/*
+1. W pliku `Main7.java` stwórz dwie pętle niezależne i wypisz wartości ich liczników w każdej iteracji.
+2. Wykorzystaj np.```System.out.println("i= " +i + " j= "+j);```
+3. Zadanie rozwiąż używając dwóch pętli `for`.
+ */
