672cd095d2848d9b8282e07cabcbfc97a2e04296
Marta Rembelska <martarembelska@pc46.home>
2018-06-26 20:57:17 +0200

    zadania

---
+++ A.pierwszeZadania/src/main/java/Zadania/coders/tablice/Main1.java
@@ -0,0 +1,23 @@
+package Zadania.coders.tablice;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int[] onlyThree= {1,2,3};
+        System.out.println(onlyThree[0]);
+        System.out.println(onlyThree[1]);
+        System.out.println(onlyThree[2]);
+
+
+
+    }
+}
+/*
+W pliku `Main1.java`
+
+1. Stwórz tablicę z **trzema** elementami np. z liczbami, zmienną z
+ tablicą nazwij `onlyThree`.
+2. Następnie wypisz na konsoli po kolei elementy tej tablicy
+rozdzielając je spacją.
+3. Nie korzystaj w tym zadaniu z pętli.
+
+ */
---
+++ A.pierwszeZadania/src/main/java/ZadaniaCoders/Prework/Petle/Main16.java
@@ -0,0 +1,23 @@
+package ZadaniaCoders.Prework.Petle;
+
+public class Main16 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+W pliku `Main16.java`
+
+1. Dodaj 2 zmiennej `x` i `y` będące dodatnimi liczbami całkowitymi.
+2. Napisz program wyliczający największy wspólny dzielnik
+(greatest common divisor - GDC) dwóch liczb.
+3. Skorzystaj z opisu na tej [stronie][GDC].
+4. Wynik wyświetl na stronie wraz z liczbami, dla których został
+wyliczony wg. wzoru `Dla liczb 420 i 168 GDC to: 84`.
+
+<!-- Links -->
+[GDC]: http://www.programming-algorithms.net/article/43434
+/Greatest-common-divisor
+
+ */
